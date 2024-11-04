49a8ab75076d2019988c80b6fab906076fb62463
David Spörri <davepermen@hotmail.com>
2024-11-05 00:04:19 +0100

    cleaning up some stuff

--- src/Features/DesktopApplication/FakeDataSource.cs
+++ src/Features/DesktopApplication/FakeDataSource.cs
@@ -1,11 +1,13 @@
 ﻿namespace Conesoft.Server_Host.Features.DesktopApplication;
 
-public record FakeDataContext(FakeWebsite[] Websites, FakeService[] Services);
-public record FakeService(string Name, string ProcessDescription, int ProcessId);
-public record FakeWebsite(string Name, string ProcessDescription, int ProcessId, ushort Port);
+
 public static class FakeDataSource
 {
-    public static FakeDataContext Context { get; private set; } = new(
+    public record FakeDataContext(FakeWebsite[] Websites, FakeService[] Services);
+    public record FakeService(string Name, string ProcessDescription, int ProcessId);
+    public record FakeWebsite(string Name, string ProcessDescription, int ProcessId, ushort Port);
+
+    public static FakeDataContext Context { get; } = new(
         Websites: [
             new("davepermen.net", "davepermen.net.exe", ProcessId: 69420, Port: 54321),
             new("conesoft.net", "conesoft.net.exe", ProcessId: 42069, Port: 12345),
