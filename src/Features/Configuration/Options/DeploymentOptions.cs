0b223eca4bc9d2d94e04ee389e55a16ada3ad4de
David Spörri <davepermen@hotmail.com>
2024-12-05 15:54:55 +0100

    Delete src/Features/Configuration directory

--- src/Features/Configuration/Options/DeploymentOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Conesoft.Server_Host.Features.Configuration.Options;
-
-class DeploymentOptions()
-{
-    public string Domain { get; init; } = "";
-
-    public string Hosting { get; init; } = "";
-}
